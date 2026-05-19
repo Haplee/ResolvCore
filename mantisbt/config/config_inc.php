a7a5dbf35f1c46f7b7946ddee8cb10bfce4b7b66
FranVi <fvidalmateo@gmail.com>
2026-05-20 00:17:35 +0200

    feat: add WordPress and MantisBT database configurations and site files

---
+++ mantisbt/config/config_inc.php
@@ -0,0 +1,14 @@
+<?php
+$g_hostname               = 'db';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'mantis';
+$g_db_username            = 'mantis';
+$g_db_password            = 'mantis';
+
+$g_default_timezone       = 'Europe/Madrid';
+
+$g_crypto_master_salt     = 'VYN83XZpOaNhKQ9C3G0J+jePI75myahTH4KW8R8rfao=';
+$g_webservice_rest_enabled = ON;
+$g_allow_anonymous_login = OFF;
+$g_webservice_rest_enabled = ON;
+$g_api_token_lifetime = 0;
