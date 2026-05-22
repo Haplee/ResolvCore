12a3f760bc754036841a4003496c455f09d93b1f
FranVi <fvidalmateo@gmail.com>
2026-05-22 13:08:51 +0200

    feat: initialize documentation, MantisBT configuration, and system diagnostic scripts

--- mantisbt/config/config_inc.php
+++ mantisbt/config/config_inc.php
@@ -8,7 +8,7 @@ $g_db_password            = 'mantis';
 $g_default_timezone       = 'Europe/Madrid';
 
 $g_crypto_master_salt     = 'VYN83XZpOaNhKQ9C3G0J+jePI75myahTH4KW8R8rfao=';
-$g_webservice_rest_enabled = ON;
+$g_allow_rest_api = ON;
 $g_allow_anonymous_login = OFF;
 $g_api_token_lifetime = 0;
 
